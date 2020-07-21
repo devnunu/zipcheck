cb7539faa952a8da6ced57e0d4b194bee30e17f8
eunwoo <aufcl4858@naver.com>
2020-07-22 00:27:17 +0900

    Create home fragment and view model

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,4 +1,17 @@
 package com.example.myapplication.ui.home
 
-class HomeFragment {
+import androidx.fragment.app.viewModels
+import com.example.myapplication.R
+import com.example.myapplication.common.BaseFragment
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
+    R.layout.fragment_home,
+    HomeViewModel::class.java
+) {
+    private val viewModel by viewModels<HomeViewModel> { viewModelFactory }
+
+    override fun setBindingVariables() {
+        super.setBindingVariables()
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.di.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+class HomeViewModel : ViewModel() {
+
+}
+
+@Module
+abstract class HomeViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeViewModel::class)
+    abstract fun bindMyViewModel(viewmodel: HomeViewModel): ViewModel
+}
