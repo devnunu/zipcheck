6d5c259525b608ddae2380d05d27bd4e28192949
eunwoo <aufcl4858@naver.com>
2020-07-26 23:50:48 +0900

    Remove unused file

--- app/src/main/java/com/example/myapplication/common/ext/TextViewExt.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.common.ext
-
-import android.view.View
-import android.widget.TextView
-
-fun TextView.visibleIf(visible: Boolean) {
-    visibility = if (visible) View.VISIBLE else View.GONE
-}
