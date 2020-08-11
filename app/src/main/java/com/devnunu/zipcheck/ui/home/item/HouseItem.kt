79e1347a6f4e97fec2e7179bf6d8ff1b00c22347
eunwoo <aufcl4858@naver.com>
2020-08-11 23:46:29 +0900

    Update home fragment design UI

--- app/src/main/java/com/devnunu/zipcheck/ui/home/item/HouseItem.kt
+++ app/src/main/java/com/devnunu/zipcheck/ui/home/item/HouseItem.kt
@@ -1,10 +1,40 @@
 package com.devnunu.zipcheck.ui.home.item
 
+import com.devnunu.zipcheck.common.util.CurrencyUtil
 import com.devnunu.zipcheck.data.house.model.House
+import com.devnunu.zipcheck.data.house.model.HouseType
 
 class HouseItem(house: House) {
 
     val id = house.id
 
     val name = house.name
+
+    val typeAndPriceText = getHousePrice(house)
+
+    val checklistCountText = getCheckStatus(house)
+
+    private fun getCheckStatus(house: House): String {
+        var totalChecklistCount = 0
+        var checkedItemCount = 0
+
+        val keys = house.checklist?.items?.keys
+        val items = house.checklist?.items
+        keys?.forEach { key ->
+            totalChecklistCount += items?.size ?: 0
+            checkedItemCount += items?.get(key)?.filter { it.isGood != null }?.size ?: 0
+        }
+
+        return "${checkedItemCount}/${totalChecklistCount}"
+    }
+
+    private fun getHousePrice(house: House): String {
+        return when (house.houseType) {
+            HouseType.LEASE_MONTHLY_PAY ->
+                "${house.houseType?.displayName} ${CurrencyUtil.toKrCurrencyText(house.deposit)}/${CurrencyUtil.toKrCurrencyText(
+                    house.monthlyPay
+                )}"
+            else -> "${house.houseType?.displayName} ${CurrencyUtil.toKrCurrencyText(house.deposit)}"
+        }
+    }
 }
