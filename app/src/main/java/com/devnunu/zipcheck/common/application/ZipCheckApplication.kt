1dbf14af3efdbc93ba73cacc931c36418b8cdb76
devnunu <aufcl4858@naver.com>
2023-03-04 17:38:08 +0900

    refactor: hilt/mavericks 삭제 및 orbit 추가

--- app/src/main/java/com/devnunu/zipcheck/common/application/ZipCheckApplication.kt
+++ app/src/main/java/com/devnunu/zipcheck/common/application/ZipCheckApplication.kt
@@ -4,11 +4,8 @@ import android.app.Application
 import com.airbnb.mvrx.Mavericks
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
 class ZipCheckApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
-        Mavericks.initialize(this)
     }
 }
