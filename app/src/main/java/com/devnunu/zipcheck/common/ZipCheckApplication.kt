0d2e63153b10c8f415d9e861a69c72f6a6131ee0
devnunu <aufcl4858@naver.com>
2023-03-04 16:54:51 +0900

    feat: 매버릭스 라이브러리 추가

--- app/src/main/java/com/devnunu/zipcheck/common/ZipCheckApplication.kt
+++ app/src/main/java/com/devnunu/zipcheck/common/ZipCheckApplication.kt
@@ -1,8 +1,14 @@
 package com.devnunu.zipcheck.common
 
 import android.app.Application
+import com.airbnb.mvrx.Mavericks
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ZipCheckApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Mavericks.initialize(this)
+    }
 }
