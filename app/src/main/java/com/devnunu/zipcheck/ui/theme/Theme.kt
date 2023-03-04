e8657beadc26ed4da315198519dbad4859393cb1
devnunu <aufcl4858@naver.com>
2023-03-04 16:49:11 +0900

    first commit

--- app/src/main/java/com/devnunu/zipcheck/ui/theme/Theme.kt
+++ app/src/main/java/com/devnunu/zipcheck/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ZipcheckTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun ZipCheckTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
